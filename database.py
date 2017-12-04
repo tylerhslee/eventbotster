3be0287ae245feaea46f8da70020465049a7f3b4
tylerhslee <hyuanree@gmail.com>
2017-12-04 12:07:55 -0500

    Modified AnyEventOnDayIntent handler to use the database

--- database.py
+++ database.py
@@ -1,25 +1,12 @@
 
-# coding: utf-8
-
-# In[208]:
-
-
-import urllib.request, json 
+import urllib.request, json
 import pandas as pd
 import MySQLdb as mdb
 
-
-# In[152]:
-
-
 con = mdb.connect(host = 'localhost',
-                  user = 'root', 
-                  passwd = 'dwdstudent2015', 
+                  user = 'root',
+                  passwd = 'dwdstudent2015',
                   charset='utf8', use_unicode=True);
-
-
-# In[153]:
-
 
 # Run a query to create a database that will hold the data
 db_name = 'final_project'
@@ -30,19 +17,15 @@ cursor = con.cursor()
 cursor.execute(create_db_query)
 cursor.close()
 
-
-# In[193]:
-
-
 cursor = con.cursor()
-table_name = 'events_bot'
+table_name = 'event_bot'
 create_table_query = '''CREATE TABLE IF NOT EXISTS {db}.{table}
                                 (title VARCHAR(100),
                                 url VARCHAR(1000),
-                                time VARCHAR(100),  
+                                start_time VARCHAR(100),
                                 category VARCHAR(100),
-                                min_price VARCHAR(100), 
-                                max_price VARCHAR(100), 
+                                min_price VARCHAR(100),
+                                max_price VARCHAR(100),
                                 zip_code VARCHAR(100),
                                 venue VARCHAR(100),
                                 lon VARCHAR(100),
@@ -51,16 +34,13 @@ create_table_query = '''CREATE TABLE IF NOT EXISTS {db}.{table}
                                 )'''.format(db=db_name, table=table_name)
 cursor.execute(create_table_query)
 cursor.close()
-
-
-
-# In[199]:
+ In[199]:
 
 
 def extract_columns(json):
     title = json['_embedded']['events'][0]['name']
     url = str(json['_embedded']['events'][0]['url'])
-    time = json['_embedded']['events'][0]['dates']['start']['dateTime']
+    start_time = json['_embedded']['events'][0]['dates']['start']['dateTime']
     category = json['_embedded']['events'][0]['classifications'][0]['segment']['name']
     min_price = str(json['_embedded']['events'][0]['priceRanges'][0]['min'])
     max_price = str(json['_embedded']['events'][0]['priceRanges'][0]['max'])
@@ -68,79 +48,69 @@ def extract_columns(json):
     venue = json['_embedded']['events'][0]['_embedded']['venues'][0]['name']
     lon = json['_embedded']['events'][0]['_embedded']['venues'][0]['location']['longitude']
     lat = json['_embedded']['events'][0]['_embedded']['venues'][0]['location']['latitude']
-    
-    
+
+
     extracted_json = {
-    'title': [title],
-    'url': [str(url)],
-    'time': time,
-    'category': category,
-    'min_price': min_price,
-    'max_price': max_price,
-    'zip_code': zip_code,
-    'venue': venue,
-    'lon': lon,
-    'lat': lat
+        'title': title,
+        'url': str(url),
+        'start_time': start_time,
+        'category': category,
+        'min_price': min_price,
+        'max_price': max_price,
+        'zip_code': zip_code,
+        'venue': venue,
+        'lon': lon,
+        'lat': lat
     }
 
     return extracted_json
 
 
-# In[191]:
-
-
 def store_data(json):
-    
-    dataframe = pd.DataFrame(extract_columns(json))
- 
     query_template = '''INSERT INTO {db}.{table}(title,
-                                            url, 
-                                            time,
+                                            url,
+                                            start_time,
                                             category,
                                             min_price,
                                             max_price,
                                             zip_code,
                                             venue,
                                             lon,
-                                            lat) 
+                                            lat)
                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''.format(db=db_name, table=table_name)
     cursor = con.cursor()
 
-    for index, row in dataframe.iterrows():
-        title = dataframe['title'][0]
-        url = dataframe["url"][0]
-        category = dataframe["category"][0]
-        min_price = float(dataframe["min_price"])
-        max_price = float(dataframe["max_price"])
-        zip_code = dataframe['zip_code'][0]
-        venue = dataframe['venue'][0]
-        lon = dataframe['lon'][0]
-        lat = dataframe['lat'][0]
-        query_parameters = (title, url, time, category, min_price, max_price, zip_code, venue, lon, lat)
-        cursor.execute(query_template, query_parameters)
-    
+    title = json['title']
+    url = json["url"]
+    start_time = json['start_time']
+    category = json["category"]
+    min_price = json["min_price"]
+    max_price = json["max_price"]
+    zip_code = json['zip_code']
+    venue = json['venue']
+    lon = json['lon']
+    lat = json['lat']
+    query_parameters = (title, url, start_time, category, min_price, max_price, zip_code, venue, lon, lat)
+    cursor.execute(query_template, query_parameters)
+
+    con.commit()
+    cursor.close()
 
 
-        con.commit()
-        cursor.close()
-    
+def find_table(**kwargs):
+    f = ['%s=%s' % (k, kwargs[k]) for k in dict(kwargs).keys()]
+    query = '''SELECT * FROM `final_project`.`event_bot` WHERE %s'''.format(' AND '.join(f))
+    cursor = con.cursor()
+    cursor.execute(query)
+    cursor.commit()
+    data = cursor.fetch()
+    cursor.close()
+    return data
 
 
-# In[209]:
-
-
-#Example of Usage
 if __name__ == '__main__':
     with urllib.request.urlopen("https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=80Yiy3BkF0kupWGVzo75UtuPdt1Vu1V3&startDateTime=2017-12-09T00:12:00Z") as url:
         json = json.loads(url.read().decode())
 
     dataframe = extract_columns(json)
     dataframe
-
-
-
-# In[ ]:
-
-
-
-
--- handlers.py
+++ handlers.py
@@ -4,8 +4,9 @@
 Handles each intent defined in examples.txt
 """
 from parser import keywords, parse_keywords
-from dates import DAYS_OF_WEEK, MODIFIERS, date_diff, add_days 
+from dates import DAYS_OF_WEEK, MODIFIERS, date_diff, add_days
 from tm import get_info
+from database import find_table, store_data
 
 
 def any_event_on_day_intent_handler(doc):
@@ -18,17 +19,23 @@ def any_event_on_day_intent_handler(doc):
             modifier = MODIFIERS[tok.text]
         if tok.text in parse_keywords(keywords, 'dayOfWeek'):
             dow = DAYS_OF_WEEK[tok.text]
-    
+
     diff = date_diff(modifier, dow)
     target_date = add_days(diff)  # Add timezone info
 
-    r = get_info(date=target_date)
-    return r.json()
+    data = find_table(start_date=target_date)
+    if len(data.keys()) == 0:
+        r = get_info(date=target_date)
+        store_data(r.json())
+        return r.json()
+    return data
 
 
 def specific_event_on_day_intent_handler(doc):
     """
     Handles SpecificEventOnDayIntent
+    Always fetches the API because we can't store
+    "keyword" column in the database.
     """
     modifier = MODIFIERS['this']
     for tok in doc:
@@ -44,4 +51,3 @@ def specific_event_on_day_intent_handler(doc):
 
     r = get_info(date=target_date, keyword=et)
     return r.json()
-
